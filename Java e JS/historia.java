1be18d34cc2cde40cc2de314e4c589ccc6e91a2e
Guilherme <165524574+itsguisouza@users.noreply.github.com>
2024-11-27 12:58:34 -0300

    Update historia.java

--- Java e JS/historia.java
+++ Java e JS/historia.java
@@ -38,8 +38,7 @@ public class Pessoa {
 // Execução Assíncrona
 public class Main extends Thread {
     public void run() {
-    System.out.println("Tarefa emsegundo plano em
-    Java!");
+    System.out.println("Tarefa emsegundo plano em Java!");
     }
     public static void main(String[] args) {
     Main tarefa = new Main();
